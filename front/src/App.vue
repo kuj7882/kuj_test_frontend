37e2383bad474fc892012d5855f2ba7f5a219c3d
youjin0409 <kuh7882@naver.com>
2025-02-26 10:00:51 +0900

    [Feat] 파일 추가가

---
+++ front/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+import BoardList from "./board/BoardList.vue";
+import  from "./components/Main.vue";
+import BoardList from "./components/Sidebar.vue";
+
+export default {
+  components: { Header, Main, Sidebar },
+};
+</script>
